059f5de00a425e5d5c6edc7540ab6ae361788137
Raffy <rikscen@gmail.com>
2019-03-12 11:27:19 +0800

    Added ex24 and ex25

---
+++ ex25run.py
@@ -0,0 +1,17 @@
+import ex25
+sentence = "The quick brown fox jumps over the lazy dog."
+words = ex25.break_words(sentence)
+print ex25.break_words(sentence)
+print "\n"
+print ex25.sort_words(words)
+print "\n"
+print ex25.print_first_word(words)
+print "\n"
+print ex25.print_last_word(words)
+print "\n"
+print ex25.sort_sentence(sentence)
+print "\n"
+print ex25.print_first_and_last(sentence)
+print "\n"
+print ex25.print_first_and_last_sorted(sentence)
+
